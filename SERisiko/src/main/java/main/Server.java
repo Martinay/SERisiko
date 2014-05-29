35a491c29b555928691a2a85ca5caf8e4a444d3d
n-e-r-v <nerv@blutnacht.com>
2014-05-29 13:47:16 +0200

    add auto docRoot

--- SERisiko/src/main/java/main/Server.java
+++ SERisiko/src/main/java/main/Server.java
@@ -1,6 +1,7 @@
 package main;
 
 import ServerApi.RisikoServer;
+import java.net.URL;
 import org.webbitserver.WebServer;
 import org.webbitserver.WebServers;
 import org.webbitserver.handler.StaticFileHandler;
@@ -12,12 +13,17 @@ public class Server
 {
     public static void main( String[] args )
     {
+        URL serverLocation = Server.class.getProtectionDomain().getCodeSource().getLocation();
+
+        //add devFix
+        String docRoot = serverLocation.getPath() + "../../web";
+        
         WebServer webServer = WebServers.createWebServer(80)
                 .add("/websocket", new RisikoServer()) //add Risiko Api Service 
-                .add(new StaticFileHandler("C:\\share\\risk\\JsRemotetest\\web")); //add http response Service
+                .add(new StaticFileHandler( docRoot )); //add http response Service
         webServer.start();
         System.out.println("Server running at " + webServer.getUri());
-        
+        System.out.println("WebRoot: " + docRoot);
         System.out.print("Connected clients: 0");
     }
 }
