6cf063504eb55ad12f1231c7d3af5ab55030ec54
Krypt0n <a.s.losada-@gmx.de>
2014-05-30 22:18:03 +0200

    added test map inclusive mouse click/out/over events new class structure

---
+++ SERisiko/web/js/Main.js
@@ -0,0 +1,328 @@
+// ---------------------- #
+
+/**
+ *
+ * @author Alexander Santana Losada
+ */
+
+var Main = new Main();
+
+function Main() {
+    //#Public Vars
+    this.gameList = new GameList();
+    this.sctTable = new SelectableTable(this.gameList);
+    
+    //#Private Vars
+    var thePlayerName = "";
+    var game = "stopped";
+    var gameListRefresher = $.timer(function(){Main.updateGameList();}, 2000);
+    var playerListRefresher = $.timer(function(){Main.updatePlayerList();}, 2000);
+    
+    //#InitConnection Function
+    var connection = null;
+    var etablishConnection = function(){
+        if (connection != null)
+            connection.close();
+        connection = new RisikoApi();
+
+        // Start ConnectionToServer
+        connection.onmessage = function(elem) { //get message from server
+            console.log( elem );
+            parseServerAnswers(elem);
+            $('#serverAnswers').append("Serveranswer: " + elem.data + "<br>");
+        };
+    };
+    etablishConnection();
+    //# Load Map... done by html object    
+
+    //# Public Methods
+    this.setPlayerName = function(){
+        var name = document.getElementById("playerName").value;
+        if(name == "" || name == "Ihr Spielername")
+            return false;
+        thePlayerName = name;
+
+        // create Player on Server + joinLobby
+        connection.joinServer(thePlayerName); // includes joinlobby
+        //connection.joinLobby(thePlayerName);
+        
+        //wait for answer
+        showElement(document.getElementById("loading_overlay"));
+    };
+
+    this.deletePlayerName = function(){
+        if(thePlayerName == "")
+            return false;
+
+        connection.leaveLobby();
+        thePlayerName = "";
+        playerNameRegistered = false;
+        document.getElementById("playerName").value = "Ihr Spielername";
+
+        // revert menu
+        showElement(document.getElementById("setPlayerName"));
+        hideElement(document.getElementById("selectGame"));
+        gameListRefresher.pause();
+
+        divs = document.getElementsByClassName('playerNameDisplay');
+        [].slice.call(divs).forEach(function(div){div.innerHTML = playerName;});
+        this.sctTable.clear("availableGames");
+        
+        etablishConnection();
+    };
+
+    this.backToLobby = function(){
+        connection.leaveGame();
+        //check response
+        connection.joinLobby();
+        
+        showElement(document.getElementById("selectGame"));
+        hideElement(document.getElementById("game"));
+        hideElement(document.getElementById("newGame"));
+        game = "stopped";
+        gameListRefresher.play();
+        playerListRefresher.pause();
+    };
+
+    this.setGame = function(id){
+        if(this.sctTable != null){
+            //if(this.sctTable.isSelected()){
+            if(id > 0 && id <= this.gameList.getGameAmount() && id <= this.sctTable.getRows()){
+                //alert("Joining to " + document.getElementById('cell_'+this.sctTable.getSelectedRow()+',1').innerHTML + " ...");
+                //alert("Joining to " + document.getElementById('cell_'+id+',1').innerHTML + " ...");
+                joinGame(this.sctTable, id);
+            }
+            else
+                alert("You must select a Game first!");
+        }
+        else
+            alert("Error! no gameTable");
+    };
+
+    this.showCreateNewGame = function(){
+        showElement(document.getElementById("newGame"));
+        hideElement(document.getElementById("selectGame"));
+        gameListRefresher.pause();
+    };
+
+    this.createNewGame = function(){
+        //check game settings....
+        var gameName = document.getElementById("gameName").value;
+        var maxPlayers = document.getElementById("maxPlayers").value;
+        var gameSettings = document.getElementById("gameSettings").value;
+        //parse data to server
+        connection.createGame();
+        //verify 
+        var idFromServer = 123;
+        this.setGame(idFromServer);
+    };
+
+    this.updateGameList = function(){
+        if(this.sctTable != null){
+            this.sctTable.clear("availableGames");
+            getGameList(this.sctTable, this.gameList);
+        }
+        else
+            alert("Error! no gameTable");	
+    };
+
+    this.minmax = function(value, min, max){
+        if(parseInt(value) < min || isNaN(value)) 
+            return min; 
+        else if(parseInt(value) > max) 
+            return max; 
+        else return value;
+    };
+
+    this.checkForKey = function(caller, key){
+        switch(caller){
+            case "playerName":
+                if(key == 13)
+                    this.setPlayerName();
+                break;
+            default:
+                    //nothing
+        }
+    };
+
+    this.readyToPlay = function(){		
+        // send to server : player ready
+        // connection.setPlayerReady(); not yet implemented
+        this.updatePlayerList();
+    };
+
+    this.updatePlayerList = function(){
+        // get playerlsit from server // get ready state of pl from server
+        connection.listPlayers();
+        // parse message
+        var players = [ // pseudo test data
+                        {"name" : "Hans von Massow", "rdy" : 1},
+                        {"name" : "Maism�ller", "rdy" : 1},
+                        {"name" : "Karl-Heinz", "rdy" : 0},
+                        {"name" : "Philipp", "rdy" : 1},
+                        {"name" : "Alex", "rdy" : 0},
+                        {"name" : "Nerv", "rdy" : 1}
+                        ];			  	  
+        var rdy = '<img id="Ready" src="img/Gruener-Haken.jpg" width="15" align="right"/>';
+        var notRdy = '';
+
+        document.getElementById("PlayerList").innerHTML = "";
+        for(var i = 0; i < 6; i++){
+            $("#PlayerList").append(players[i].name + ((players[i].rdy == 1)? rdy : notRdy) + "<br>");
+        }
+    };
+    
+    this.clearServerAnswers = function(){
+        document.getElementById("serverAnswers").innerHTML = "";
+    };
+    
+    //# Private Methods    
+    var joinGame = function(table, id){
+        showElement(document.getElementById("game"));
+        hideElement(document.getElementById("selectGame"))
+        gameListRefresher.pause();
+        game = "running";
+        table.selectRow(0);
+        
+        playerListRefresher.play();
+        
+        connection.leaveLobby();
+        connection.joinGame(id);
+    };
+    var hideElement = function(element){
+        element.style.display = "none";
+    };
+    var showElement = function(element){
+        element.style.display = "block";
+    };
+
+    var getGameList = function(table, list){
+        // get gameslist from server
+        connection.listOpenGames();
+        
+        list.addGame(["Game 1", "DHBW", "1/6", "n/a"], table);
+        list.addGame(["Game 42", "World", "0/6", "n/a"], table);
+        list.addGame(["Game 3", "DHBW", "4/6", "n/a"], table);
+    };
+    
+    var parseServerAnswers = function(elem){
+        var message = JSON.parse(elem.data);
+        
+        switch(message.type){
+            case "AddNewPlayerToLobbyMessage":
+                if(message.state == 1){
+                    hideElement(document.getElementById("loading_overlay"));      
+
+                    // create Player on Client
+                    hideElement(document.getElementById("setPlayerName"));
+                    showElement(document.getElementById("selectGame"));
+                    divs = document.getElementsByClassName('playerNameDisplay');
+                    [].slice.call(divs).forEach(function(div){div.innerHTML = thePlayerName;});
+
+                    gameListRefresher.play();
+                }
+                else{
+                    alert("Error: Bad response from Server");
+                    hideElement(document.getElementById("loading_overlay"));                    
+                }
+                break;
+            default:
+                //nothing
+        }  
+    };
+    
+    var sleep = function(milliseconds) {
+      var start = new Date().getTime();
+      for (var i = 0; i < 1e7; i++) {
+        if ((new Date().getTime() - start) > milliseconds){
+          break;
+        }
+      }
+    };
+}
+
+function GameWindow(cv, ct){
+    //#Public Vars
+
+    //#Private Vars
+    var canvas = cv;
+    var ctx = ct;
+
+    //# Public Methods
+    this.clear = function(){
+        // clear stuff
+    };
+
+    //# Private Methods
+}
+
+function GameList(){
+    //#Public Vars
+
+    //#Private Vars
+    var amount = 0;
+    var games = new Array();
+
+    //# Public Methods
+    this.clear = function(){
+        games = [];
+        amount = 0;
+    };
+
+    this.addGame = function(data, table){
+        table.addRow("availableGames", data);
+        amount++;
+        games.push(parseData(data));
+    };
+
+    this.getGames = function(){
+        if(games != null)
+            return games;
+        else
+            alert('no games found');
+    };
+
+    this.getGameAmount = function(){
+        return amount;
+    };
+
+    this.getGame = function(index){
+        return games[index];
+    };
+
+    //# Private Methods
+    var parseData = function(data){
+        var game = new GameObject(data[0], data[1], data[2], data[3]);
+        return game;
+    };
+}
+
+function GameObject(name, number, mp, maxP, set){
+    //#Public Vars
+
+    //#Private Vars
+    var gameName = name;
+    var gameId = number;
+    var map = mp;
+    var maxPlayers = maxP;
+    var settings = set;
+
+    //# Public Methods
+    this.getGameName = function(){
+        return gameName;
+    };
+    this.getMaxPlayers = function(){
+        return maxPlayers;
+    };
+    this.getSettings = function(){
+        return settings;
+    };
+    this.getMap = function(){
+        return map;
+    };
+    this.getGameId = function(){
+        return gameId;
+    }
+    //# Private Methods
+}
+// ---------------------- #
---
+++ SERisiko/web/js/SvgFunctions.js
@@ -0,0 +1,25 @@
+// ---------------------- #
+
+/**
+ *
+ * @author Alexander Santana Losada
+ */
+
+var SvgFunctions = new SvgFunctions();
+
+function SvgFunctions(){
+    //#Public Vars    
+    //#Private Vars
+
+    //# Public Methods
+    this.svgRectIn = function(rect){
+        alert("rect"); 
+        
+    };
+    
+    this.svgRectOut = function(rect){
+        
+        
+    };
+    //# Private Methods
+}
