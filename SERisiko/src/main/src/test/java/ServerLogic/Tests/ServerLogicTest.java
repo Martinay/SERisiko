81887f5ddc3339e2deae04befdef49b1b80fd527
Martinay <martinay@outlook.com>
2014-06-03 11:40:56 +0200

    Added LogicTests

---
+++ SERisiko/src/main/src/test/java/ServerLogic/Tests/ServerLogicTest.java
@@ -0,0 +1,10 @@
+package ServerLogic.Tests;
+
+import junit.framework.TestCase;
+
+public class ServerLogicTest extends TestCase {
+
+    public void abd() throws Exception {
+     assertEquals(true,false);
+    }
+}
