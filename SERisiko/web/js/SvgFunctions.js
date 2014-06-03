f6940e06ece4372a72af8db9951c31c728366880
Philipp <Philipp@141.72.121.200>
2014-06-03 14:02:17 +0200

    Svg - Attack Style

--- SERisiko/web/js/SvgFunctions.js
+++ SERisiko/web/js/SvgFunctions.js
@@ -19,11 +19,8 @@ function SvgFunctions(){
     
     //#Private Vars
     var svgDoc = null;
-    var scaleLevel = 1;
-    var scaleAmp = 0.1;
-    var movAmp = 50;
-    var xPos = 0;
-    var yPos = 0;
+    var neighborLands = null;
+    var myLands = new Array();
     
     var countAttack = 0;
     var attacker = "";
@@ -31,89 +28,27 @@ function SvgFunctions(){
     
     //# Public Methods
     this.init = function(doc){
-        scaleLevel = 1;
-        scaleAmp = 0.1;
-        movAmp = 50;
-        xPos = 0;
-        yPos = 0;
+        var i = 0;
         
         svgDoc = doc;
         rects = svgDoc.getElementsByTagName("rect");
         [].slice.call(rects).forEach(function(rect){
-            rect.onmouseover = new Function("this.setAttribute('opacity', '0.75'); this.style='cursor: pointer';");
-            rect.onmouseout = new Function("this.setAttribute('opacity','1'); this.style='cursor: default';");
-            //rect.onclick = new Function("Core.svgHandler.setUnit(this.id, 10);");
-            rect.onclick = new Function("Core.svgHandler.attack(this.id);");
+            if(rect.getAttribute("Owner") === "Ihr SpielernaG"){
+                rect.onmouseover = new Function("this.setAttribute('opacity', '0.75'); this.style='cursor: pointer';");
+                rect.onmouseout = new Function("this.setAttribute('opacity','1'); this.style='cursor: default';");
+                //rect.onclick = new Function("Core.svgHandler.setUnit(this.id, 10);");
+                rect.onclick = new Function("Core.svgHandler.attack(this.id);");
+                myLands[i] = rect.getAttribute("id");
+                i++;
+            }
         });
     };
         
-    this.zoomOut = function(){
-        var scale_tmp = scaleLevel;
-        scaleLevel -= scaleAmp;
-        
-        xPos = -(scale_tmp-scaleLevel)*(getCenter()[0]/3.54);
-        yPos = -(scale_tmp-scaleLevel)*(getCenter()[1]/3.54);
-        
-        transform();
-        
-        //svgSetScale(svgDoc, scaleLevel, scale_tmp);
-    };
-    this.zoomIn = function(){
-        var scale_tmp = scaleLevel;
-        scaleLevel += scaleAmp;
-        
-        xPos = -(scale_tmp-scaleLevel)*(getCenter()[0]/3.54);
-        yPos = -(scale_tmp-scaleLevel)*(getCenter()[1]/3.54);
- 
-        transform();
-    };
-    this.moveLeft = function(){
-        xPos += movAmp;
-        transform();
-    };
-    this.moveRight = function(){
-        xPos -= movAmp;
-        transform();
-    };
-    this.moveUp = function(){
-        yPos += movAmp;
-        transform();
-    };
-    this.moveDown = function(){
-        yPos -= movAmp;
-        transform();
-    };
-    
-    /*
-    function svgSetScale(svg, amount, oldAmount){ 
-        var box = svg.getBBox(); 
-        var cx = box.x + box.width/2; 
-        var cy = box.y + box.height/2; 
-        //$(element).attr("data-scale", amount); 
-        
-        svg.setAttribute("transform", "translate(" + cx + " " + cy + ") scale(" + amount + ") translate(" + (-cx) + " " + (-cy) + ")"); 
-        
-        var diffAmount = amount / oldAmount; 
-        
-        var scrollX = $('#svg-container').scrollLeft(); 
-        var scrollY = $('#svg-container').scrollTop(); 
-        var middle = getScrollCenter(); 
-        
-        var diffX = scrollX - middle[0]; 
-        var diffY = scrollY - middle[1]; 
-        
-        var diffActualX = (diffAmount * diffX) - diffX; 
-        var diffActualY = (diffAmount * diffY) - diffY; 
-        
-        $('#svg-container').scrollLeft(scrollX + diffActualX); 
-        $('#svg-container').scrollTop(scrollY + diffActualY); 
-    }*/
-    
     this.setUnit = function(id, count){
         var theRect = svgDoc.getElementById(id);
 
-        var xPosition = parseInt(theRect.getAttribute("x")) + theRect.getAttribute("width")/2;
-        var yPosition = parseInt(theRect.getAttribute("y")) + theRect.getAttribute("height")/2;
+        var xPosition = parseInt(theRect.getAttribute("x")) + 10;
+        var yPosition = parseInt(theRect.getAttribute("y")) + 10;
 
         var addUnits = svgDoc.getElementById("MapUnit");
 
@@ -132,27 +67,60 @@ function SvgFunctions(){
                         count = count - 1;
                 }
             }
+                
+            if((xPosition + 1800) < (parseInt(theRect.getAttribute("x")) + parseInt(theRect.getAttribute("width")))){
+                xPosition = xPosition + 910;
+            } else {
+                if((yPosition + 2010) < (parseInt(theRect.getAttribute("y")) + theRect.getAttribute("height"))){
+                    yPosition = yPosition + 1010;
+                }
+            }
         }
-    }
+    };
 
     this.attack = function(id){
         var theRect = svgDoc.getElementById(id);
 
         if(countAttack === 0){
+            neighborLands = theRect.getAttribute("neighbor").split(",");
             attacker = id;
             theRect.onmouseout = new Function("this.setAttribute('opacity','0.5');");
-            this.setUnit(id, 1);
+            //this.setUnit(id, 16);
             countAttack++;
-        } else {  
-            defender = id;
-            theRect = svgDoc.getElementById(attacker);
-            theRect.onmouseout = new Function("this.setAttribute('opacity','1');");
-            theRect.setAttribute('opacity','1');
-            countAttack = 0;
-            alert("Von: " + attacker + " Nach: " + defender);
-            showAttack();
+            for (var i = 0; i < myLands.length; i++) {
+                theRect = svgDoc.getElementById(myLands[i]);
+                theRect.onmouseover = new Function("");
+                theRect.onmouseout = new Function("");
+                theRect.onclick = new Function("");
+            }
+            for (var i = 0; i < neighborLands.length; i++) {
+                theRect = svgDoc.getElementById(neighborLands[i]);
+                if(theRect.getAttribute("Owner") !=  "Ihr SpielernaG"){
+                    theRect.onmouseover = new Function("this.setAttribute('opacity', '0.75'); this.style='cursor: pointer';");
+                    theRect.onmouseout = new Function("this.setAttribute('opacity','1'); this.style='cursor: default';");
+                    theRect.onclick = new Function("Core.svgHandler.attack(this.id);");
+                }
+            }
+        } else { 
+            for (var i = 0; i < neighborLands.length; i++) {
+                if(id === neighborLands[i]){
+                    defender = id;
+                    theRect = svgDoc.getElementById(attacker);
+                    theRect.onmouseout = new Function("this.setAttribute('opacity','1');");
+                    theRect.setAttribute('opacity','1');
+                    countAttack = 0;
+                    showAttack();
+                }
+            }
+            if(countAttack === 1){
+                countAttack = 0;
+                theRect = svgDoc.getElementById(attacker);
+                theRect.onmouseout = new Function("this.setAttribute('opacity','1');");
+                theRect.setAttribute('opacity','1');
+                Alert("Dieser Angriff ist nicht möglich!!!");
+            }
         }
-    }
+    };
 
 
     //# Private Methods
@@ -171,5 +139,7 @@ function SvgFunctions(){
         document.getElementById("loading_overlay").style.display = "block";
         var OverlayString = '<div id="ShowAttack">\n<table>\n<tr>\n<td>Attacker:</td>\n<td>Defender:</td>\n</tr>\n<tr>\n<td>\n<img id="AttackPNG1" alt="Attack" src="img/paper.png" height="150"><br />\n<img id="AttackPNG2" alt="Attack" src="img/paper.png" height="150"><br />\n<img id="AttackPNG3" alt="Attack" src="img/paper.png" height="150">\n</td>\n<td>\n<img id="DefendPNG1" alt="Defend" src="img/paper.png" height="150"><br />\n<img id="DefendPNG2" alt="Defend" src="img/paper.png" height="150">\n</td>\n</tr>\n</table>\n</div>\n';
         setTimeout(function(){document.getElementById("loading_overlay").innerHTML = OverlayString;},2000);
-    }
+        setTimeout(function(){document.getElementById("loading_overlay").style.display = "none";},10000);
+        setTimeout(function(){document.getElementById("loading_overlay").innerHTML = '<div id="loading_message">Waiting for Server... <img id="loading" alt="Loading Screen" src="img/loading_overlay.gif"></div>';},10000);
+    };
 }
