d83e88e1f1c11aeaa2a88062ba0e5fb31603eeb1
frogger1883 <simwetz@hotmail.de>
2014-07-11 09:59:11 +0200

    Update Spielwelt.java
    verteile Laender ohne spieler x
    Hoffe es geht, konnte es nur im Webbrowser bearbeiten, da ich bei der Arbeit bin.
    => pls Feedback

--- SERisiko/src/main/java/GameLogic/Spielwelt.java
+++ SERisiko/src/main/java/GameLogic/Spielwelt.java
@@ -48,6 +48,19 @@ public class Spielwelt {
 		return anzahl;
 	}
 	
+	public void verteile_neu_ohne(Spieler zuentfernenderspieler, Spieler[] andereSpieler){
+		pos=0;
+		for (int i=0; i<dieLaender.length; i++){
+			if (dieLaender[i].gib_besitzer()=zuentfernenderspieler){
+				dieLaender[i].neuerBesitzer(andereSpieler[pos]);
+				pos++;
+				while(dieLaender[i].decArmeen){}
+				dieLaender[i].mehr_Armeen(1);
+			}
+			if (pos==andereSpieler.length) pos=0;
+		}
+	}
+	
 	protected int gib_anz_Laender(Spieler derSpieler){
 		int anzahl = 0;
 		
